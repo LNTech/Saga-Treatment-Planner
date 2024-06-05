ec63b4eb372f28dd5f7661c90c7883d34d4fd598
Luke Oliver <lo106@student.le.ac.uk>
2024-06-05 21:35:33 +0100

    Changed filenames+added credits

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
-from app import app
+from flaskapp import app
 from extensions import db
+
+__author__ = "Luke Oliver"
+__version__ = "1.0.0"
+
 
 if __name__ == "__main__":
     with app.app_context():
