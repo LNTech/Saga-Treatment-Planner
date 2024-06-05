4b7b34b2841f7321a8eec49b67f0300b8cf59e56
Luke Oliver <lo106@student.le.ac.uk>
2024-06-05 21:43:41 +0100

    Fixed database not being created in gunicorn

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,9 @@ from extensions import db
 __author__ = "Luke Oliver"
 __version__ = "1.0.0"
 
+# Make database and tables
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run()
